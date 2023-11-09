8d2d0841fa379e140781da3ca2289544e98b849c
kabir-afk <kabirsingh12b@gmail.com>
2023-11-09 19:57:16 +0530

    1st Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
 import './App.css';
-
 function App() {
+  const ipAddress = "103.164.25.251";
+  const url = `https://freeipapi.com/api/json/${ipAddress}`;
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">Hello world </div>
+      <div id="map"></div>
+    </>
   );
 }
 
